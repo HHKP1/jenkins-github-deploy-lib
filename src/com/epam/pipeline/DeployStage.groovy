00c34641067df28da5f5e6739403c70b94d8c565
HHKP <41621991+HHKP1@users.noreply.github.com>
2024-03-25 20:08:40 +0200

    upd caching

--- src/com/epam/pipeline/DeployStage.groovy
+++ src/com/epam/pipeline/DeployStage.groovy
@@ -20,6 +20,6 @@ class DeployStage implements Serializable {
     }
 
     def run() {
-        script.sh "docker run -d --expose ${containerPort} -p ${hostPort}:${containerPort} -v ~/.npm:~/.npm --name ${containerName} ${registry}/${dockerImageName}:${dockerImageTag}"
+        script.sh "docker run -d --expose ${containerPort} -p ${hostPort}:${containerPort} -v ~/.npm:/root/.npm --name ${containerName} ${registry}/${dockerImageName}:${dockerImageTag}"
     }
 }
